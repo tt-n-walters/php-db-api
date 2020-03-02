884e2bf6b658d89fe3fc16937f358f62fd8fae97
Nico <nico.walters@techtalents.es>
2020-03-02 18:33:50 +0100

    Added core Configuration and Connection classes

---
+++ core/Configuration.php
@@ -0,0 +1,75 @@
+<?php
+
+class Configuration {
+
+    private $connection = array(
+        "host" => null,
+        "databaseName" => null,
+        "username" => null,
+        "password" => null
+    );
+
+    private $permissions = array(
+        "select" => true,
+        "insert" => true,
+        "update" => true,
+        "delete" => true,
+        "create" => true,
+        "drop" => true
+    );
+
+    public static function fromFile(string $filename) {
+        if (file_exists($filename)) {
+            $file = file($filename, FILE_IGNORE_NEW_LINES) or exit("Unable to open credentials file.");
+
+            return new Configuration(...$file);
+        }
+    }
+
+    public function __construct(
+        string $host,
+        string $database,
+        string $username,
+        string $password,
+        boolean $select = true,
+        boolean $insert = true,
+        boolean $update = true,
+        boolean $delete = true,
+        boolean $create = true,
+        boolean $drop = true,
+    ) {
+        $this->connection["host"] = $host;
+        $this->connection["database"] = $database;
+        $this->connection["username"] = $username;
+        $this->connection["password"] = $password;
+        $this->permissions["select"] = $select,
+        $this->permissions["insert"] = $insert,
+        $this->permissions["update"] = $update,
+        $this->permissions["delete"] = $delete,
+        $this->permissions["create"] = $create,
+        $this->permissions["drop"] = $drop,
+    }
+
+    public function saveToFile(string $filename) {
+        $connection = array_values($this->connection);
+        $permissions = array_values($this->per$permissions);
+
+        file_put_contents($filename, implode("\n", array_merge($connection, $permissions));
+    }
+
+    public function getConnectionHost() {
+        return $this->connection["host"];
+    }
+
+    public function getConnectionDatabase() {
+        return $this->connection["database"];
+    }
+
+    public function getConnectionUsername() {
+        return $this->connection["username"];
+    }
+
+    public function getConnectionPassword() {
+        return $this->connection["password"];
+    }
+}
---
+++ core/Connection.php
@@ -0,0 +1,31 @@
+<?php
+
+class Connection {
+
+    private $configuration;
+    private $connection;
+
+    public function __construct(Configuration $configuration) {
+        $this->configuration = $configuration;
+
+        try {
+            $this->connection = new mysqli(
+                $configuration->getConnectionHost(),
+                $configuration->getConnectionDatabase(),
+                $configuration->getConnectionUsername(),
+                $configuration->getConnectionPassword(),
+            );
+        } catch (mysqli_sql_exception $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function getDatabase() {
+        return new Database($this, $this->configuration->getDatabaseName);
+    }
+
+    public function performQuery(BaseQuery $query) {
+        $result = $query->execute($this);
+        return $result;
+    }
+}
